e8741519597ede26952e47bf82b99cc16cd0c1a7
BitterBlue22 <telisax@gmail.com>
2020-08-04 13:28:18 +0100

    adds mongodb and bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const apiRouter = require("./routers/apiRouter");
+const MongoClient = require("mongodb").MongoClient;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/", apiRouter);
+
+MongoClient.connect("mongodb-connection-string", (err, client) => {
+  // ...
+});
 
 app.listen(3000, function () {
   console.log("listening on 3000");
